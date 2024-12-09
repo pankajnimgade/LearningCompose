d1f4b08073775e025282ce8bbb9dc9f464ed8048
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2024-12-08 22:29:09 -0600

    updated

--- app/src/main/java/com/example/composetest101/compose/test111/Compose111Activity.kt
+++ app/src/main/java/com/example/composetest101/compose/test111/Compose111Activity.kt
@@ -4,16 +4,25 @@ package com.example.composetest101.compose.test111
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,44 +49,74 @@ class Compose111Activity : ComponentActivity() {
 }
 
 
-/*********************************************************************************/
-
+/**MyApp Start*******************************************/
 @Composable
-private fun MyApp(
-    modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")
+private fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen() {
+            shouldShowOnboarding = false
+        }
+    } else {
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, name = "MyApp Preview", widthDp = 320, heightDp = 320)
+@Composable
+fun ShowPreviewOfMyApp() {
+    MyApp()
+}
+/**MyApp End*******************************************/
+
+/**Greetings Start*******************************************/
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "Item: $it" }
 ) {
     Surface(
         modifier = modifier, color = MaterialTheme.colorScheme.background
     ) {
-        Column(modifier = modifier.padding(vertical = 4.dp)) {
-            for (item in names) {
-                Greeting(item)
+        LazyColumn {
+            items(names) {
+                Greeting(it)
             }
         }
     }
 }
 
-@Preview(showBackground = true, name = "MyApp Preview", widthDp = 320)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun ShowPreviewOfMyApp() {
-    MyApp()
+fun ShowPreviewOfGreetings() {
+    Greetings()
 }
+/**Greetings End*******************************************/
 
-/*********************************************************************************/
 
+/**Greeting Start*******************************************/
 @Composable
 private fun Greeting(text: String = "Android", modifier: Modifier = Modifier) {
+    /**
+     * [remember] is used to guard against recomposition, so the state is not reset.
+     */
+    var expanded by remember { mutableStateOf(false) }
+    var extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = modifier.padding(24.dp)) {
-            Column(modifier = modifier.weight(1f)) {
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello")
                 Text(text = "$text!", modifier = modifier)
             }
-            ElevatedButton(onClick = {}) {
-                Text(text = "Show more")
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
 
@@ -95,5 +134,40 @@ fun ShowPreviewOfGreeting() {
         }
     }
 }
+/**Greeting End*******************************************/
 
-/*********************************************************************************/
+/**OnBoardingScreen Start*******************************************/
+@Composable
+private fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelabs!")
+        Button(
+            modifier = modifier
+                .padding(24.dp)
+                .wrapContentSize(),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true, name = "Onboarding Screen Preview", widthDp = 320, heightDp = 320
+)
+@Composable
+fun ShowPreviewOfOnboardingScreen() {
+    ComposeTest101Theme {
+        OnboardingScreen() {
+
+        }
+    }
+}
+/**OnBoardingScreen End*******************************************/
