a7a98208a52157e96f775550fb95a4127c00dbe4
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2024-12-09 20:58:31 -0600

    update

--- app/src/main/java/com/example/composetest101/compose/test112/Compose112Activity.kt
+++ app/src/main/java/com/example/composetest101/compose/test112/Compose112Activity.kt
@@ -1,12 +1,15 @@
 package com.example.composetest101.compose.test112
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -24,6 +27,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetest101.R
+import com.example.composetest101.compose.test102.ui.theme.ComposeTest101Theme
 
 /**
  * https://developer.android.com/develop/ui/compose/tutorial
@@ -43,34 +47,57 @@ private data class Message(val author: String, val body: String)
 
 @Composable
 private fun MessageCard(message: Message = Message("Android", "Jetpack Compose")) {
+    ComposeTest101Theme {
+        Surface {
+            Row(modifier = Modifier
+                .padding(all = 8.dp)
+                .fillMaxWidth()) {
+                Image(
+                    painter = painterResource(R.drawable.profile_picture),
+                    contentDescription = "Contact profile picture",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        // Set image size to 40 dp
+                        .size(40.dp)
+                        // Clip image to be shaped as a circle
+                        .clip(CircleShape)
+                        .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
 
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                .padding(8.dp)
-                // Set image size to 40 dp
-                .size(40.dp)
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-
-        )
-        MySpace()
-        Column {
-            Text(text = message.author, style = MaterialTheme.typography.bodyMedium)
-            MySpace()
-            Text(text = message.body, style = MaterialTheme.typography.bodyMedium)
+                )
+                MySpace()
+                Column {
+                    Text(
+                        text = message.author,
+                        color = MaterialTheme.colorScheme.secondary,
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                    MySpace()
+                    Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                        Text(
+                            text = message.body,
+                            modifier = Modifier.padding(all = 4.dp),
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
         }
     }
-
 }
 
-@Preview(showBackground = true, widthDp = 200, heightDp = 200)
+@Preview(showBackground = true, widthDp = 250, heightDp = 150)
+@Preview(
+    showBackground = true,
+    widthDp = 250,
+    heightDp = 150,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
 fun ShowPreviewOfMessageCard() {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        MessageCard(message = Message("Lexy", "Hey, take a look at Jetpack Compose"))
+    ComposeTest101Theme {
+        Surface(color = MaterialTheme.colorScheme.primary) {
+            MessageCard(message = Message("Lexy", "Hey, take a look at Jetpack Compose"))
+        }
     }
 }
 
