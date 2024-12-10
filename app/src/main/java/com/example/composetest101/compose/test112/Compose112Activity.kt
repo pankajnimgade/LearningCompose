a9420e426bd50ad61658cb0bb2570d69920dc653
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2024-12-09 21:37:40 -0600

    update

--- app/src/main/java/com/example/composetest101/compose/test112/Compose112Activity.kt
+++ app/src/main/java/com/example/composetest101/compose/test112/Compose112Activity.kt
@@ -4,8 +4,12 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,6 +27,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -72,6 +80,10 @@ data class Message(val author: String, val body: String)
 
 @Composable
 private fun MessageCard(message: Message = Message("Android", "Jetpack Compose")) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val surfaceColor by animateColorAsState(
+        if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+    )
     ComposeTest101Theme {
         Surface {
             Row(modifier = Modifier
@@ -90,17 +102,22 @@ private fun MessageCard(message: Message = Message("Android", "Jetpack Compose")
 
                 )
                 MySpace()
-                Column {
+                Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
                     Text(
                         text = message.author,
                         color = MaterialTheme.colorScheme.secondary,
                         style = MaterialTheme.typography.titleSmall
                     )
                     MySpace()
-                    Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                    Surface(
+                        color = surfaceColor,
+                        shape = MaterialTheme.shapes.medium,
+                        shadowElevation = 1.dp,
+                        modifier = Modifier.animateContentSize().padding(1.dp)) {
                         Text(
                             text = message.body,
                             modifier = Modifier.padding(all = 4.dp),
+                            maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                             style = MaterialTheme.typography.bodyMedium
                         )
                     }
